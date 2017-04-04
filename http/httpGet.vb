2d75435235d7b3d33e518bc279938a29003ec794
Phil Wood <phil@pwood.net>
2017-04-04 22:29:43 +0100

    First commit

---
+++ http/httpGet.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Net
+Imports System.IO
+
+Module httpGet
+
+    Sub Main()
+
+        Dim Request As HttpWebRequest
+        Dim Response As WebResponse
+        Dim Reader As StreamReader
+        Dim Line As String
+
+        Request = WebRequest.Create("http://chart.finance.yahoo.com/table.csv?s=MKS.L&ignore=.csv")
+        Response = Request.GetResponse()
+
+        Reader = New StreamReader(Response.GetResponseStream())
+        Line = Reader.ReadLine()
+
+        While Line IsNot Nothing
+            Console.WriteLine(Line)
+            Line = Reader.ReadLine()
+        End While
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
